a8222e7a2a620ea11195d47a5417dc4b302e6ff0
Jonathan Halterman <jhalterman@gmail.com>
2011-07-10 16:33:44 -0700

    build cleanup

--- push_javadoc.sh
+++ push_javadoc.sh
@@ -2,7 +2,7 @@
 rm -rf docs
 git clone git@github.com:jhalterman/modelmapper.git docs -b gh-pages
 cd core
-mvn javadoc:javadoc
+mvn -Pjavadoc javadoc:javadoc
 cd ../docs
 git add -A
 git commit -m "updated javadocs"
