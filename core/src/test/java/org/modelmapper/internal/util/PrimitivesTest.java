d15811b368b78d235af37af1ccbc86520cd8c801
Jonathan Halterman <jhalterman@doradosoftware.com>
2011-07-27 17:40:31 -0700

    test fix

--- core/src/test/java/org/modelmapper/internal/util/PrimitivesTest.java
+++ core/src/test/java/org/modelmapper/internal/util/PrimitivesTest.java
@@ -15,21 +15,13 @@ import org.testng.annotations.Test;
 @Test
 public class PrimitivesTest {
   public void testDefaultValue() {
-    assertEquals(Primitives.defaultValue(Byte.class), (byte) 0);
     assertEquals(Primitives.defaultValue(Byte.TYPE), (byte) 0);
-    assertEquals(Primitives.defaultValue(Short.class), (short) 0);
     assertEquals(Primitives.defaultValue(Short.TYPE), (short) 0);
-    assertEquals(Primitives.defaultValue(Integer.class), 0);
     assertEquals(Primitives.defaultValue(Integer.TYPE), 0);
-    assertEquals(Primitives.defaultValue(Long.class), 0L);
     assertEquals(Primitives.defaultValue(Long.TYPE), 0L);
-    assertEquals(Primitives.defaultValue(Float.class), 0.0f);
     assertEquals(Primitives.defaultValue(Float.TYPE), 0.0f);
-    assertEquals(Primitives.defaultValue(Double.class), 0.0d);
     assertEquals(Primitives.defaultValue(Double.TYPE), 0.0d);
-    assertEquals(Primitives.defaultValue(Character.class), '\u0000');
     assertEquals(Primitives.defaultValue(Character.TYPE), '\u0000');
-    assertEquals(Primitives.defaultValue(Boolean.class), false);
     assertEquals(Primitives.defaultValue(Boolean.TYPE), false);
     
     assertNull(Primitives.defaultValue(List.class));
