a6e4ba4b438851d8669f73337b3b766faa8f60f7
Jonathan Halterman <jhalterman@gmail.com>
2012-07-24 15:33:30 -0700

    Updated circular dependency test for skipped properties

--- core/src/test/java/org/modelmapper/functional/circular/CircularDependencies1.java
+++ core/src/test/java/org/modelmapper/functional/circular/CircularDependencies1.java
@@ -1,8 +1,10 @@
 package org.modelmapper.functional.circular;
 
-import static org.testng.Assert.assertNull;
+import static org.testng.Assert.*;
 
 import org.modelmapper.AbstractTest;
+import org.modelmapper.ConfigurationException;
+import org.modelmapper.PropertyMap;
 import org.testng.annotations.Test;
 
 /**
@@ -21,8 +23,68 @@ public class CircularDependencies1 extends AbstractTest {
     A value;
   }
 
+  static class Tree {
+    Node node;
+  }
+
+  static class Node {
+    Tree tree;
+    String value;
+  }
+
+  static class DTree {
+    DNode node;
+
+    public DNode getNode() {
+      return node;
+    }
+  }
+
+  static class DNode {
+    DTree tree;
+    String value;
+
+    public void setTree(DTree tree) {
+      this.tree = tree;
+    }
+  }
+
   public void shouldNotThrowOnMatchingCircularReference() {
     B b2 = modelMapper.map(new A(), B.class);
     assertNull(b2.value);
+  }
+
+  @Test(expectedExceptions = ConfigurationException.class)
+  public void shouldThrowOnNonSkippedCircularReference() {
+    modelMapper.map(new Tree(), DTree.class);
+  }
+
+  public void shouldAllowSkippedCircularReference() {
+    modelMapper.addMappings(new PropertyMap<Tree, DTree>() {
+      @Override
+      protected void configure() {
+        skip().getNode().setTree(null);
+      }
+    });
+
+    modelMapper.map(new Tree(), DTree.class);
+  }
+
+  // Asserts that the Node->DNode mapping is merged into the Tree->DTree mapping, and skipped
+  public void shouldAllowSkippedCircularReferenceForInnerProperty() {
+    modelMapper.addMappings(new PropertyMap<Node, DNode>() {
+      @Override
+      protected void configure() {
+        skip().setTree(null);
+      }
+    });
+
+    Tree tree = new Tree();
+    tree.node = new Node();
+    tree.node.value = "test";
+    DTree dt = modelMapper.map(tree, DTree.class);
+
+    assertNull(dt.node.tree);
+    assertEquals(dt.node.value, "test");
   }
 }
