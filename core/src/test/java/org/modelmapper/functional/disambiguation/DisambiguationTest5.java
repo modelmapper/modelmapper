8baa57fe4f70f76553ed1b6ff9f554189391aa7e
Jonathan Halterman <jhalterman@gmail.com>
2012-07-24 11:43:46 -0700

    Added disambiguation test for generics

---
+++ core/src/test/java/org/modelmapper/functional/disambiguation/DisambiguationTest5.java
@@ -0,0 +1,85 @@
+package org.modelmapper.functional.disambiguation;
+
+import static org.testng.Assert.assertEquals;
+
+import org.modelmapper.AbstractTest;
+import org.modelmapper.Provider;
+import org.testng.annotations.Test;
+
+/**
+ * @author https://github.com/andy-m
+ */
+@Test(groups = "functional")
+public class DisambiguationTest5 extends AbstractTest {
+  static class Source {
+    Prop<?> someValue = new StringProp("Some String");
+  }
+
+  static abstract class Prop<T> {
+    public abstract T getValue();
+
+    public abstract void setValue(T value);
+  }
+
+  static class StringProp extends Prop<String> {
+    String value;
+
+    public StringProp(final String value) {
+      this.value = value;
+    }
+
+    @Override
+    public String getValue() {
+      return value;
+    }
+
+    @Override
+    public void setValue(final String value) {
+      this.value = value;
+    }
+  }
+
+  static class Dest {
+    DestProp<?> someValue;
+
+    public void setSomeValue(final DestProp<?> someValue) {
+      this.someValue = someValue;
+    }
+  }
+
+  static abstract class DestProp<T> {
+    public abstract T getValue();
+
+    public abstract void setValue(T value);
+  }
+
+  static class DestStringProp extends DestProp<String> {
+    String value;
+
+    @Override
+    public void setValue(final String value) {
+      this.value = value;
+    }
+
+    @Override
+    public String getValue() {
+      return value;
+    }
+  }
+
+  static class DestPropProvider implements Provider<Object> {
+    public Object get(ProvisionRequest<Object> request) {
+      if (DestProp.class.equals(request.getRequestedType()))
+        return new DestStringProp();
+      return null;
+    }
+  }
+
+  public void shouldAllowMappingOfNestedPropertiesInComplexGenericTypes() {
+    Source source = new Source();
+    modelMapper.getConfiguration().setProvider(new DestPropProvider());
+    Dest d = modelMapper.map(source, Dest.class);
+
+    assertEquals(source.someValue.getValue(), d.someValue.getValue());
+  }
+}
