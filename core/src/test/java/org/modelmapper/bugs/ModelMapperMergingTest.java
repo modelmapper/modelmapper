546d6d564efc09e5d75b44dc751f917a011e604b
Jonathan Halterman <jhalterman@gmail.com>
2012-09-12 09:02:15 -0700

    WIP

---
+++ core/src/test/java/org/modelmapper/bugs/ModelMapperMergingTest.java
@@ -0,0 +1,58 @@
+package org.modelmapper.bugs;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.modelmapper.AbstractTest;
+import org.testng.annotations.Test;
+
+public class ModelMapperMergingTest extends AbstractTest {
+  public static class TestItem {
+    TestItemConfiguration testItemConfiguration;
+  }
+
+  static class TestItemConfiguration {
+    SomeConfiguration someConfiguration;
+  }
+
+  static class SomeConfiguration {
+    String someProperty;
+    boolean someOtherProperty;
+  }
+
+  static class TestItemDTO {
+    TestItemConfigurationDTO testItemConfiguration;
+  }
+
+  static class TestItemConfigurationDTO {
+    SomeConfigurationDTO someConfiguration;
+  }
+
+  static class SomeConfigurationDTO {
+    String someProperty;
+  }
+
+  @Test
+  public void testValuesInDestinationRemainUnchangedUnlessTheyArePresentInSource() {
+    String expectedValue = "someValue";
+    TestItemDTO source = new TestItemDTO();
+    source.testItemConfiguration = new TestItemConfigurationDTO();
+    source.testItemConfiguration.someConfiguration = new SomeConfigurationDTO();
+    source.testItemConfiguration.someConfiguration.someProperty = "someValue";
+
+    TestItem dest = new TestItem();
+    TestItemConfiguration destItemConfig = new TestItemConfiguration();
+    SomeConfiguration destSomeConfig = new SomeConfiguration();
+    dest.testItemConfiguration = destItemConfig;
+    dest.testItemConfiguration.someConfiguration = destSomeConfig;
+    dest.testItemConfiguration.someConfiguration.someProperty = "someValueX";
+    dest.testItemConfiguration.someConfiguration.someOtherProperty = true;
+
+    modelMapper.map(source, dest);
+
+    assertEquals(expectedValue, dest.testItemConfiguration.someConfiguration.someProperty);
+    assertEquals(destItemConfig, dest.testItemConfiguration);
+    assertEquals(destSomeConfig, dest.testItemConfiguration.someConfiguration);
+    assertTrue(dest.testItemConfiguration.someConfiguration.someOtherProperty);
+  }
+}
