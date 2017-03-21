d4830aae46e06790cc4c8338d082055b91869535
Jonathan Halterman <jhalterman@gmail.com>
2017-03-21 16:57:34 -0700

    Simplify javadoc generation scripts

--- bin/push-javadoc.sh
+++ bin/push-javadoc.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-# run from bin dir
-cd ../
-rm -rf docs
-git clone git@github.com:jhalterman/modelmapper.git docs -b gh-pages
-cd core
-mvn -Pjavadoc javadoc:javadoc
-cd ../docs
-git add -A
-git commit -m "updated javadocs"
-git push origin gh-pages
+# Pushes javadocs for current build
+# Run from top level dir
+
+mvn javadoc:javadoc
+rm -rf target/docs
+git clone git@github.com:modelmapper/modelmapper.github.io.git target/docs > /dev/null
+cd target/docs
+git rm -rf javadoc
+mkdir -p javadoc
+mv -v ../../core/target/site/apidocs/* javadoc
+git add -A -f javadoc
+git commit -m "Updated JavaDocs"
+git push -fq origin master > /dev/null
+
+echo "Published Javadoc to modelmapper.github.io.\n"
