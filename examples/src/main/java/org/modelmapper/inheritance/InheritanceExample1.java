a349ed446fb1a0efa57dd3eea5889206f2677622
Michael Krausse <michael.krausse@directbox.com>
2017-06-21 21:42:14 +0200

    ehmkah: repaired git rebase mess :-(

--- examples/src/main/java/org/modelmapper/inheritance/InheritanceExample1.java
+++ examples/src/main/java/org/modelmapper/inheritance/InheritanceExample1.java
@@ -1,10 +1,10 @@
 package org.modelmapper.inheritance;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.Provider;
 import org.modelmapper.TypeMap;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -12,22 +12,25 @@ import static org.testng.Assert.assertTrue;
 public class InheritanceExample1 {
 
     public static void main(String... args) {
-        BaseSrcA baseSrcA = new BaseSrcA();
-        BaseSrcB baseSrcB = new BaseSrcB();
-        List<BaseSrc> bases = new ArrayList<BaseSrc>();
-        bases.add(baseSrcA);
-        bases.add(baseSrcB);
-        C c = new C(bases);
+        org.modelmapper.inheritance.C c = new org.modelmapper.inheritance.C(Arrays.asList(new BaseSrcA(),  new BaseSrcB()));
+
 
         ModelMapper modelMapper = new ModelMapper();
-        modelMapper.createTypeMap(BaseSrcA.class, BaseDestA.class);
-        modelMapper.createTypeMap(BaseSrcB.class, BaseDestB.class);
 
-        TypeMap<BaseSrc, BaseDest> typeMap = modelMapper.createTypeMap(BaseSrc.class, BaseDest.class);
+        TypeMap<BaseSrc, BaseDest> typeMap = modelMapper.createTypeMap(BaseSrc.class, BaseDest.class)
+                .include(BaseSrcA.class, BaseDestA.class)
+                .include(BaseSrcB.class, BaseDestB.class);
 
-        typeMap.include(BaseSrc.class, BaseDest.class)
-                .include(BaseSrcB.class, BaseDestB.class)
-                .include(BaseSrcA.class, BaseDestA.class);
+        modelMapper.typeMap(BaseSrcA.class, BaseDest.class).setProvider(new Provider<BaseDest>() {
+            public BaseDest get(ProvisionRequest<BaseDest> request) {
+                return new BaseDestA();
+            }
+        });
+        modelMapper.typeMap(BaseSrcB.class, BaseDest.class).setProvider(new Provider<BaseDest>() {
+            public BaseDest get(ProvisionRequest<BaseDest> request) {
+                return new BaseDestB();
+            }
+        });
 
         CcDTO ccDTO = modelMapper.map(c, CcDTO.class);
 
