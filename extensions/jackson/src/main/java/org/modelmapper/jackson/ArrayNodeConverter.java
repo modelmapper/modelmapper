9fe01cd54cf06c93c933b0ca4f27b687e2563dc2
chhsiao90 <chhsiao90@gmail.com>
2018-06-01 11:52:45 +0800

    Adds utility class for mapping context
    To prevent modelmapper modules from using TypeResolver directly,
    and to reduce the redundant code, so I create a utility class
    that can be used for: creating collection destination and resolve
    the destination element type with giver mapping context.

--- extensions/jackson/src/main/java/org/modelmapper/jackson/ArrayNodeConverter.java
+++ extensions/jackson/src/main/java/org/modelmapper/jackson/ArrayNodeConverter.java
@@ -1,13 +1,9 @@
 package org.modelmapper.jackson;
 
 import java.lang.reflect.ParameterizedType;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
+import org.modelmapper.internal.util.MappingContextHelper;
 import org.modelmapper.internal.util.Types;
 import org.modelmapper.spi.ConditionalConverter;
 import org.modelmapper.spi.Mapping;
@@ -16,8 +12,6 @@ import org.modelmapper.spi.PropertyInfo;
 import org.modelmapper.spi.PropertyMapping;
 
 import com.fasterxml.jackson.databind.node.ArrayNode;
-
-import net.jodah.typetools.TypeResolver;
 
 /**
  * Converts {@link ArrayNode} instances to {@link Collection} instances.
@@ -40,17 +34,15 @@ public class ArrayNodeConverter implements ConditionalConverter<ArrayNode, Colle
     int sourceLength = source.size();
     Collection<Object> destination = context.getDestination() == null ? createDestination(context, sourceLength)
         : context.getDestination();
-    Class<?> elementType = getElementType(context);
+    Class<?> elementType = MappingContextHelper.resolveCollectionElementType(context);
 
-    int index = 0;
     for (Object sourceElement : source) {
       Object element = null;
       if (sourceElement != null) {
         MappingContext<?, ?> elementContext = context.create(sourceElement, elementType);
         element = context.getMappingEngine().map(elementContext);
       }
-
-      setElement(destination, element, index);
+      destination.add(element);
     }
 
     return destination;
@@ -58,31 +50,6 @@ public class ArrayNodeConverter implements ConditionalConverter<ArrayNode, Colle
 
   private Collection<Object> createDestination(
       MappingContext<ArrayNode, Collection<Object>> context, int length) {
-    if (context.getDestinationType().isInterface())
-      if (SortedSet.class.isAssignableFrom(context.getDestinationType()))
-        return new TreeSet<Object>();
-      else if (Set.class.isAssignableFrom(context.getDestinationType()))
-        return new HashSet<Object>();
-      else
-        return new ArrayList<Object>(length);
-
-    return context.getMappingEngine().createDestination(context);
-  }
-
-  private Class<?> getElementType(MappingContext<ArrayNode, Collection<Object>> context) {
-    Mapping mapping = context.getMapping();
-    if (mapping instanceof PropertyMapping) {
-      PropertyInfo destInfo = mapping.getLastDestinationProperty();
-      Class<?> elementType = TypeResolver.resolveRawArgument(destInfo.getGenericType(),
-          destInfo.getInitialType());
-      return elementType == TypeResolver.Unknown.class ? Object.class : elementType;
-    } else if (context.getGenericDestinationType() instanceof ParameterizedType)
-      return Types.rawTypeFor(((ParameterizedType) context.getGenericDestinationType()).getActualTypeArguments()[0]);
-
-    return Object.class;
-  }
-
-  private void setElement(Collection<Object> destination, Object element, int index) {
-    destination.add(element);
+    return MappingContextHelper.createCollection(context, length);
   }
 }
