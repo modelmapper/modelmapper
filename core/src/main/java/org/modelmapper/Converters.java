e002961cdc514dd41d979a1675f5c1b25a8c1330
chhsiao90 <chhsiao90@gmail.com>
2018-08-30 16:24:12 +0800

    Provides helper function to convert first item of collection
    Resolved: #389

--- core/src/main/java/org/modelmapper/Converters.java
+++ core/src/main/java/org/modelmapper/Converters.java
@@ -45,6 +45,33 @@ public class Converters {
   }
 
   /**
+   * Provides interface to chain another converter or convert to different destination type.
+   *
+   * @param <S> the source type
+   * @param <D1> the intermediate destination type
+   */
+  public interface ChainableConverter<S, D1> {
+
+    /**
+     * Chains a converter of {@code S} -> {@code D1} to {@code S} -> {@code D2}.
+     *
+     * @param destinationType the final destination type
+     * @param <D2> the final destination type
+     * @return a converter
+     */
+    <D2> org.modelmapper.Converter<S, D2> to(Class<D2> destinationType);
+
+    /**
+     * Chains a converter of {@code S} -> {@code D1} to {@code S} -> {@code D2} with given converter.
+     *
+     * @param destinationType the final destination type
+     * @param <D2> the final destination type
+     * @return a converter
+     */
+    <D2> org.modelmapper.Converter<S, D2> map(org.modelmapper.Converter<D1, D2> converter);
+  }
+
+  /**
    * Provides helper methods to create converters for collection.
    */
   public static class Collection {
@@ -61,31 +88,67 @@ public class Converters {
      * @param <D> the destination element type
      * @return a Converter
      */
-    public static <S, D> org.modelmapper.Converter<java.util.Collection<S>, java.util.Collection<D>> map(Converter<S, D> elementConverter) {
-      return new CollectionConverter<S, D>(elementConverter);
+    public static <S, D> org.modelmapper.Converter<java.util.Collection<S>, java.util.Collection<D>> map(final Converter<S, D> elementConverter) {
+      return new org.modelmapper.Converter<java.util.Collection<S>, java.util.Collection<D>>() {
+        @Override
+        public java.util.Collection<D> convert(
+            MappingContext<java.util.Collection<S>, java.util.Collection<D>> context) {
+          if (context.getSource() == null)
+            return null;
+
+          java.util.Collection<D> destination = MappingContextHelper.createCollection(context);
+          for (S element : context.getSource()) {
+            destination.add(elementConverter.convert(element));
+          }
+          return destination;
+        }
+      };
+    }
+
+    /**
+     * Provides a helper method to create a collection converter that will get the first element of source collect and convert it.
+     *
+     * <pre>
+     *   using(Collection.first().to(Destination.class).map();
+     * </pre>
+     *
+     * @param <S> the source type
+     * @return a {@code ChainableConverter}
+     */
+    public static <S> ChainableConverter<java.util.Collection<S>, S> first() {
+      return new ChainableConverter<java.util.Collection<S>, S>() {
+        @Override
+        public <D> org.modelmapper.Converter<java.util.Collection<S>, D> to(Class<D> destinationType) {
+          return new org.modelmapper.Converter<java.util.Collection<S>, D>() {
+            @Override
+            public D convert(MappingContext<java.util.Collection<S>, D> context) {
+              if (context.getSource() == null || context.getSource().isEmpty())
+                return null;
+              MappingContext<S, D> elementContext = context.create(context.getSource().iterator().next(),
+                  context.getDestinationType());
+              return context.getMappingEngine().map(elementContext);
+            }
+          };
+        }
+
+        @Override
+        public <D> org.modelmapper.Converter<java.util.Collection<S>, D> map(
+            final org.modelmapper.Converter<S, D> converter) {
+          return new org.modelmapper.Converter<java.util.Collection<S>, D>() {
+            @Override
+            public D convert(MappingContext<java.util.Collection<S>, D> context) {
+              if (context.getSource() == null || context.getSource().isEmpty())
+                return null;
+              MappingContext<S, D> elementContext = context.create(context.getSource().iterator().next(),
+                  context.getDestinationType());
+              return converter.convert(elementContext);
+            }
+          };
+        }
+      };
     }
 
     private Collection() {
-    }
-  }
-
-  private static class CollectionConverter<S, D> implements org.modelmapper.Converter<java.util.Collection<S>, java.util.Collection<D>> {
-    private Converter<S, D> converter;
-
-    private CollectionConverter(Converter<S, D> converter) {
-      this.converter = converter;
-    }
-
-    @Override
-    public java.util.Collection<D> convert(MappingContext<java.util.Collection<S>, java.util.Collection<D>> context) {
-      if (context.getSource() == null)
-        return null;
-
-      java.util.Collection<D> destination = MappingContextHelper.createCollection(context);
-      for (S element : context.getSource()) {
-        destination.add(converter.convert(element));
-      }
-      return destination;
     }
   }
 }
---
+++ core/src/test/java/org/modelmapper/bugs/GH389.java
@@ -0,0 +1,80 @@
+package org.modelmapper.bugs;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+import org.modelmapper.AbstractTest;
+import org.modelmapper.Converters;
+import org.modelmapper.ExpressionMap;
+import org.modelmapper.builder.ConfigurableMapExpression;
+import org.modelmapper.spi.DestinationSetter;
+import org.modelmapper.spi.SourceGetter;
+import org.testng.annotations.Test;
+
+@Test
+public class GH389 extends AbstractTest {
+  static class WrapperDTO {
+    List<ItemDTO> items;
+
+    public WrapperDTO(List<ItemDTO> items) {
+      this.items = items;
+    }
+
+    public List<ItemDTO> getItems() {
+      return items;
+    }
+
+    public void setItems(List<ItemDTO> items) {
+      this.items = items;
+    }
+  }
+
+  static class ItemDTO {
+    String text;
+
+    public ItemDTO(String text) {
+      this.text = text;
+    }
+  }
+
+  static class Wrapper {
+    Item item;
+
+    public Item getItem() {
+      return item;
+    }
+
+    public void setItem(Item item) {
+      this.item = item;
+    }
+  }
+
+  static class Item {
+    String text;
+  }
+
+  public void shouldMap() {
+    modelMapper.typeMap(WrapperDTO.class, Wrapper.class)
+        .addMappings(new ExpressionMap<WrapperDTO, Wrapper>() {
+          @Override
+          public void configure(ConfigurableMapExpression<WrapperDTO, Wrapper> mapping) {
+            mapping.using(Converters.Collection.first().to(Wrapper.class)).map(
+                new SourceGetter<WrapperDTO>() {
+                  @Override
+                  public Object get(WrapperDTO source) {
+                    return source.getItems();
+                  }
+                },
+                new DestinationSetter<Wrapper, Item>() {
+                  @Override
+                  public void accept(Wrapper destination, Item value) {
+                    destination.setItem(value);
+                  }
+                });
+          }
+        });
+    Wrapper wrapper = modelMapper.map(new WrapperDTO(Collections.singletonList(new ItemDTO("foo"))), Wrapper.class);
+    assertEquals(wrapper.item.text, "foo");
+  }
+}
