9c7971b8603d0fa41ed93f8da9152fab46cc838c
merdoc97@gmail.com <merdoc97@gmail.com>
2018-11-06 16:45:31 +0200

    add to AbstractConverter method map to allow use another converters from context.

---
+++ core/src/test/java/org/modelmapper/internal/converter/AbstractConverterFeatureTest.java
@@ -0,0 +1,68 @@
+package org.modelmapper.internal.converter;
+
+
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.internal.InheritingConfiguration;
+import org.modelmapper.internal.MappingEngineImpl;
+import org.modelmapper.internal.converter.dto.ChildDTO;
+import org.modelmapper.internal.converter.dto.ChildEntity;
+import org.modelmapper.internal.converter.dto.ParentDTO;
+import org.modelmapper.internal.converter.dto.ParentEntity;
+import org.modelmapper.spi.MappingEngine;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AbstractConverterFeatureTest {
+
+    protected ModelMapper modelMapper;
+    protected InheritingConfiguration config = new InheritingConfiguration();
+    private MappingEngine engine = new MappingEngineImpl(config);
+
+    @BeforeMethod
+    protected void init() {
+        modelMapper = new ModelMapper();
+        modelMapper.addConverter(new ParentConverter());
+        modelMapper.addConverter(new ChildConverter());
+    }
+
+
+    @Test
+    public void testConverterFeature() {
+        ParentEntity entity = getParentTest();
+        ParentDTO dto = modelMapper.map(entity, ParentDTO.class);
+        Assert.assertEquals(dto.getName(), entity.getName());
+        Assert.assertEquals(dto.getSecondName(), entity.getSecondName());
+        Assert.assertEquals(dto.getChild().getChildName(), entity.getChild().getName());
+
+    }
+
+
+    private ParentEntity getParentTest() {
+        return new ParentEntity("parent", "second", new ChildEntity("child"));
+    }
+
+    private class ParentConverter extends AbstractConverter<ParentEntity, ParentDTO> {
+
+        @Override
+        protected ParentDTO convert(ParentEntity source) {
+            ParentDTO dto = new ParentDTO();
+            dto.setSecondName(source.getSecondName());
+            dto.setName(source.getName());
+            dto.setChild(map(source.getChild(), ChildDTO.class));
+            return dto;
+        }
+    }
+
+    private class ChildConverter extends AbstractConverter<ChildEntity, ChildDTO> {
+        @Override
+        protected ChildDTO convert(ChildEntity source) {
+            ChildDTO dto = new ChildDTO();
+            dto.setChildName(source.getName());
+            return dto;
+        }
+    }
+
+
+}
---
+++ core/src/test/java/org/modelmapper/internal/converter/dto/ChildEntity.java
@@ -0,0 +1,22 @@
+package org.modelmapper.internal.converter.dto;
+
+public class ChildEntity {
+
+    private String name;
+
+    public ChildEntity() {
+    }
+
+    public ChildEntity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
